d17aaeb0ec2c644a80e551a912359fe50c77f342
Desi Vanrintel <desivanrintel@Desis-iMac.local>
2026-02-10 12:07:41 +0100

    Update CORS

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Allow Next.js to communicate with FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
