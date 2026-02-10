62a8a843ddf8b77b760e12e81eacf38ed2b634b8
Desi Vanrintel <desivanrintel@Desis-iMac.local>
2026-02-10 12:51:06 +0100

    fix localhost

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+
+
 app = FastAPI()
+
+frontend_url = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 # Allow Next.js to communicate with FastAPI
 app.add_middleware(
